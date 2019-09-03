d8137028031517f6478ecb12c231620c09ad20e4
pierodesenzi <piero.desenzi@gmail.com>
2019-09-03 01:01:48 -0300

    Adição da tela de criação de usuário
    Inclui link da tela de login para a tela de criação. Registros são inseridos ao banco.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "auth#login"
+  get "cadastrar_usuario", to: "users#new"
+
+  resources :users, only: [:new, :create]
 
 end
