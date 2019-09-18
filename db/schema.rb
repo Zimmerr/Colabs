1c814c0c85a03b504a883cf0660f33f4de4bc5cd
pierodesenzi <piero.desenzi@gmail.com>
2019-09-18 16:22:11 -0300

    Criação de models e controllers Tasks e Projects
    incluí associação de modelos (has_many/belongs_to + migração)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_023757) do
+ActiveRecord::Schema.define(version: 2019_09_18_191638) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.string "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "project_id"
+    t.index ["project_id"], name: "index_tasks_on_project_id"
+  end
 
   create_table "users", force: :cascade do |t|
-    t.string "nome", default: "", null: false
+    t.string "nome"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
