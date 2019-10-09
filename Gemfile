2ca3623767004fd06af781d0d79f48ec03933d5b
gabriel.rato@ <gabriel.rato@usp.br>
2019-10-08 23:20:44 -0300

    Agora eh possivel editar projetos
    Foi adicionado ao projeto font-awesome para liberar icones
    O teste para edicao de projeto requer uso do db/seed.rb

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'jquery-rails'
 gem 'sass-rails', '~> 5.0'
 # Popper
 gem 'popper_js', '~> 1.14.5'
+# To include cool icons
+gem 'font-awesome-rails'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- features/step_definitions/edituser_steps.rb
+++ features/step_definitions/edituser_steps.rb
@@ -1,7 +1,7 @@
 Given (/^I am on the Edit User page$/) do
     visit editar_usuario_url
   end
-  
+
   When (/^I fill the edit user form with (.*) as name and (.*) as email and (.*) as password and (.*) as confirmation$/) do |name, email, password, confirmation|
     @actual_count = User.count
     fill_in "user_nome", :with => name
@@ -9,16 +9,16 @@ Given (/^I am on the Edit User page$/) do
     fill_in "user_password", :with => password
     fill_in "user_password_confirmation", :with => confirmation
   end
-  
+
   When (/^click on the 'Atualizar' button$/) do
     click_button "Atualizar"
   end
-  
+
   Then (/^the number of existent users shoulbe be the same$/) do
     User.count.should == @actual_count
   end
-  
+
   Then (/^I should see a confirmation message$/) do
     expect(page).to have_content("Usuário atualizado com sucesso!")
   end
-  
+
