dd03c47c2b19eac6d424d59c97519bc0c6e306e0
Luis Henrique Zimmermann <lhzimmermann@gmail.com>
2019-10-15 23:00:37 -0300

    Correção no model project
    Correção de erro causado devido a erro cometido por mim na hora de fazer o merge

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,7 @@ class Project < ApplicationRecord
 
     has_many :tasks
     has_many :lists
-    has_many :users, :through => :project_users
     has_many :project_users
+    has_many :users, :through => :project_users
 
 end
