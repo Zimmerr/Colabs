1fab484d5cc269f4ede61e24ebf09cb5868fead2
gabriel.rato@ <gabriel.rato@usp.br>
2019-10-20 16:37:05 -0300

    chrome fix

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,6 @@
 
 require 'cucumber/rails'
 require 'capybara'
-require 'selenium-webdriver'
 
 
 # frozen_string_literal: true
@@ -61,7 +60,7 @@ Capybara.register_driver :chrome do |app|
     options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
     Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
-
+Capybara.javascript_driver = :chrome
 # Possible values are :truncation and :transaction
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
