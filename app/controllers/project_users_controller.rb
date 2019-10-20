684da21347aa10c88f02ef018ffcfe1e60592d40
pierodesenzi <piero.desenzi@gmail.com>
2019-10-20 12:50:18 -0300

    Melhorias
    Mas ainda não conclui a tarefa

--- app/controllers/project_users_controller.rb
+++ app/controllers/project_users_controller.rb
@@ -1,12 +1,14 @@
 class ProjectUsersController < ApplicationController
 
+
+
     def edit
         @project = Project.find(params[:id])
-        @membros = User.joins(:projects)
+        @membros = User.joins(:projects).where(projects: {id: params[:id]})
     end
     
     def set_project
-        @project = Project.find(params[:id])
+        project = Project.find(params[:id])
     end
 
     def new
@@ -17,6 +19,13 @@ class ProjectUsersController < ApplicationController
     def inserir
         @user = User.find(params[:id])
         r = ProjectUser.create(:user_id=> @user, :project_id=> @project)
+    end
+
+    def create
+        puts '-----------------------------------------------------------'
+        @user = User.find(params[:id])
+        @pu = ProjectUser.create(:user_id => params[:id], :project_id => 4)
+        #redirect_to index_path
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
   root to: "auth#login"
   get 'landing', to: 'auth#login'
   get 'project_tasks', to: 'tasks#tasks'
@@ -9,6 +10,7 @@ Rails.application.routes.draw do
   get "editar_usuario", to: "users#edit"
   get "index", to: "inicial#index"
   get 'enter', to: 'auth#new'
+  post "addprojectuser", to: "project_users#create", as: "adduser"
   post 'enter', to: 'auth#create'
   delete 'logout', to: 'auth#destroy'
 
