1b46da3776bf6f6626d6669c7f48bf07798e699e
pierodesenzi <piero.desenzi@gmail.com>
2019-10-23 10:44:34 -0300

    Implementação da query para novos usuarios

--- app/controllers/project_users_controller.rb
+++ app/controllers/project_users_controller.rb
@@ -1,38 +1,15 @@
 class ProjectUsersController < ApplicationController
 
-    '''
-    def edit
-        @project = Project.find(params[:id])
-        session[:projectid] = @project.id
-        @membros = User.joins(:projects).where(projects: {id: params[:id]})
-    end
-    '''
     def edit
         session[:projectid] = params[:id]
         @membros = User.joins(:projects).where(projects: {id: params[:id]})
     end
-    '''
-    def set_project
-        @project = Project.find(params[:id])
-        session[:projectid] = @project.id
-        puts /////////////////////////set_project//////////////////////////////////////////
-        puts session[:projectid]
-    end
-    '''
 
     def new
-        @tmembros = User.all
-        @pu = ProjectUser.new
-    end
-
-    def inserir
-        @user = User.find(params[:id])
-        r = ProjectUser.create(:user_id=> @user, :project_id=> @project)
+        @tmembros = User.where('id NOT IN (?)', User.joins(:projects).where(projects: {id: 666}).select('id'))
     end
 
     def create
-        puts '-----------------------------------------------------------'
-        @user = User.find(params[:id])
         @pu = ProjectUser.create(:user_id => params[:id], :project_id => session[:projectid])
         redirect_to new_project_user_path(session[:projectid])
     end
